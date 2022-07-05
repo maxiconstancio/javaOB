cadd5e1dc9801b7e9446611a3a68b4bb175583bc
maxiconstancio <maxiconstancio@gmail.com>
2022-07-04 21:56:58 -0300

    'first commit'

---
+++ src/exercise1/exercise.java
@@ -0,0 +1,15 @@
+package exercise1;
+
+public class exercise {
+    public static void main(String[] args) {
+        int intNumber = 12;
+        long longNumber = 123456789L;
+        double dblNumber = 123.5;
+        boolean boolVar = true;
+        String strVar = "variable";
+        System.out.println(intNumber);
+        System.out.println(longNumber);
+        System.out.println(dblNumber);
+        System.out.println(boolVar);
+    }
+}
